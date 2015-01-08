a941c0dc16f456ec643d89befd7698fdaaa88098
Sargelas <DEN-08@yandex.ru>
2015-01-09 01:50:24 +0300

    Spell checker improvement

--- src/util/spell_checker.py
+++ src/util/spell_checker.py
@@ -1,13 +1,23 @@
 import enchant
 
+from enchant.utils import trim_suggestions
+from enchant.utils import levenshtein
+
 
 RUSSIAN_DICTIONARY = enchant.Dict('ru_Ru')
 
 
+def improved_levenshtein_distance(s1, s2):
+    # TODO: add word popularity to distance calculation
+    return levenshtein(s1, s2)
+
+
 def correct(word):
-    # TODO: improve best word selection
     if not RUSSIAN_DICTIONARY.check(word):
         suggestions = RUSSIAN_DICTIONARY.suggest(word)
-        return suggestions[0] if len(suggestions) > 0 else word     # words could be unknown even for spell checker
+        if len(suggestions) == 0:   # words could be unknown even for spell checker
+            return word
+
+        return trim_suggestions(word, suggestions, 1, improved_levenshtein_distance)[0]
 
     return word
